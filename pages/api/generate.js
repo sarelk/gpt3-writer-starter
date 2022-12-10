de3ab50797642c5058924a791e1e70693ba92a4e
Hydro <sarelk@gmail.com>
2022-12-10 11:57:41 +0200

    WIP

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Giving this list of songs, which 10 songs playlist that came out at 1984 would you recommend?
+
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,160 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState, useRef } from 'react';
+import { Wizard, useWizard } from 'react-use-wizard';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Your Spotify wrapped of 1984</title>
       </Head>
       <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
-        </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
+      <Wizard>
+        <Step1 />
+        <Step2 />
+        <Step3 />
+      </Wizard>
       </div>
     </div>
+  );
+};
+
+
+const Step1 = () => {
+  const { handleStep, previousStep, nextStep } = useWizard();
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  // Attach an optional handler
+  handleStep(() => {
+    alert('Going to step 2');
+  });
+
+  return (
+    <>
+            <div className="header">
+          <div className="header-title">
+            <h1>Get your Spotify wrapped playlist of 1984 </h1>
+          </div>
+          <div className="header-subtitle">
+            <h2 >Howdy! Please insert here 10 songs you like and get the your playlist of 1984</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="List of 10 songs from your spotify liked"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      <button onClick={() => previousStep()}>Previous ⏮️</button>
+      <button onClick={() => nextStep()}>Next ⏭</button>
+    </>
+  );
+};
+const Step2 = () => {
+  const { handleStep, previousStep, nextStep } = useWizard();
+
+  // Attach an optional handler
+  handleStep(() => {
+    alert('Going to step 2');
+  });
+
+  return (
+    <>
+      <button onClick={() => previousStep()}>Previous ⏮️</button>
+      <button onClick={() => nextStep()}>Next ⏭</button>
+    </>
+  );
+};
+const Step3 = () => {
+  const { handleStep, previousStep, nextStep } = useWizard();
+
+  // Attach an optional handler
+  handleStep(() => {
+    alert('Going to step 2');
+  });
+
+  return (
+    <>
+      <button onClick={() => previousStep()}>Previous ⏮️</button>
+      <button onClick={() => nextStep()}>Next ⏭</button>
+    </>
   );
 };
 
